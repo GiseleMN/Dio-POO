bc3cfb43b6a0321f3bd1e75d4ca09a74a4a12dd2
Gisele <giginisimura@gmail.com>
2023-10-07 18:58:01 -0300

    :blush: Init Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Desafio_POO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo 1", imei: "11111111111", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
